cdc7e266239e9cf89411fe5d5015b713320ce0ce
transblanco <revolutionlucky@gmail.com>
2025-09-28 20:29:10 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+const CONFIG = {
+  githubRepo: "/transblanco/GALAXIA",   // 👈 reemplaza con tu repo real
+  branch: "main",
+  filePath: "data/galaxia.json",    // 👈 archivo que vas a leer/escribir
+};
+
+export default CONFIG;
